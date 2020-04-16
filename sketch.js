5f7f2eca8b3dceb8433b1ecb40a521c4518d8786
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-04-15 21:43:12 -0700

    3

--- sketch.js
+++ sketch.js
@@ -1,180 +1,58 @@
-/*jslint browser: true*/
-/*global $, jQuery, alert*/
 
-var gameBoardSize = 40;
-var gamePoints = 0;
-var gameSpeed = 100;
+let snake;
+let rez = 50;
+let food;
+let w;
+let h;
 
-document function (){}
-    console.log("Ready Player One!");
-    createBoard();
-    $(".btn").click(function() {
-        startGame();
-    });
-
-
-var Snake = {
-    position: [[20, 20], [20, 19], [20, 18]], // snake start position
-    size: 3,
-    direction: "r",
-    alive: true
+function setup() {
+  createCanvas(800, 800);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
 }
 
-var Food = {
-    position: [],
-    present: false
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
 }
 
-function createBoard() {
-    $("#gameBoard").empty();
-    var size = gameBoardSize;
-
-    for (i = 0; i < size; i++) {
-        $("#gameBoard").append('<div class="row"></div>');
-        for (j = 0; j < size; j++) {
-            $(".row:last-child").append('<div class="pixel"></div>');
-        }
-    }
-}
-
-function moveSnake() {
-  var head = Snake.position[0].slice();
-
-  switch (Snake.direction) {
-    case 'r':
-      head[1] += 1;
-      break;
-    case 'l':
-      head[1] -= 1;
-      break;
-    case 'u':
-      head[0] -= 1;
-      break;
-    case 'd':
-      head[0] += 1;
-      break;
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
   }
 
-  // check after head is moved
-  if (alive(head)) {
-    // draw head
-    $(".row:nth-child(" + head[0] + ") > .pixel:nth-child(" + head[1] + ")").addClass("snakePixel");
+}
 
-    // draw rest of body loop
-    for (var i = 0; i < Snake.size; i++) {
-      $(".row:nth-child(" + Snake.position[i][0] + ") > .pixel:nth-child(" + Snake.position[i][1] + ")").addClass("snakePixel");
-    }
-
-    // if head touches food
-    if (head.every(function(e,i) {
-      return e === Food.position[i];
-    })) {
-      Snake.size++;
-      Food.present = false;
-      gamePoints += 5;
-      $(".row:nth-child(" + Food.position[0] + ") > .pixel:nth-child(" + Food.position[1] + ")").removeClass("foodPixel");
-      $("#score").html("Score: "+gamePoints)
-        if (gamePoints % 16 == 0 && gameSpeed > 10) { gameSpeed -= 5; };
-    } else {
-      $(".row:nth-child(" + Snake.position[Snake.size-1][0] + ") > .pixel:nth-child(" + Snake.position[Snake.size-1][1] + ")").removeClass("snakePixel");
-      Snake.position.pop();
-    }
-    Snake.position.unshift(head);
-  } else {
-    gameOver();
+function draw() {
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+    foodLocation();
   }
-}
+  snake.update();
+  snake.show();
 
 
-function generateFood() {
-    if (Food.present === false) {
-        Food.position = [Math.floor((Math.random()*40) + 1), Math.floor((Math.random()*40) + 1)]
-        Food.present = true;
-        console.log("Food at: "+Food.position);
-        $(".row:nth-child(" + Food.position[0] + ") > .pixel:nth-child(" + Food.position[1] + ")").addClass("foodPixel");
-    }
-}
-
-function keyPress() {
-    $(document).one("keydown", function(key) {
-        switch(key.which) {
-            case 37: // left arrow
-                if (Snake.direction != "r") {Snake.direction = "l";}
-                break;
-            case 38: // up arrow
-                if (Snake.direction != "d") {Snake.direction = "u";}
-                break;
-            case 39: // right arrow
-                if (Snake.direction != "l") {Snake.direction = "r";}
-                break;
-            case 40: // down arrow
-                if (Snake.direction != "u") {Snake.direction = "d";}
-                break;
-        }
-    });
-}
-
-function gameLoop() {
-    setTimeout(function() {
-        keyPress();
-        generateFood();
-        moveSnake();
-        if (Snake.alive) {gameLoop(); }
-    }, gameSpeed);
-}
-
-function alive(head) {
-  // head check
-  if (head[0] < 1 || head[0] > 40 || head[1] < 1 || head[1] > 40) {
-    return false;
+  if (snake.endGame()) {
+    print("END GAME");
+    background(0);
+    noLoop();
   }
-  // wall collision
-  if (Snake.position[0][0] < 1 || Snake.position[0][0] > 40 || Snake.position[0][1] < 1 || Snake.position[0][1] > 40) {
-    return false;
-  }
-  // self collision
-  for (var i = 1; i < Snake.size; i++) {
-    if ((Snake.position[0]).every(function(element,index) {
-      return element === Snake.position[i][index];
-    })) {
-      return false;
-    }
-  }
-  return true;
-}
 
-function gameOver() {
-    Snake.alive = false;
-    console.log("Game Over!");
-    $(".overlay").show();
-    $("#gameOver").show();
-    var blink = function() {
-        $(".row:nth-child(" + Snake.position[0][0] + ") > .pixel:nth-child(" + Snake.position[0][1] + ")").toggleClass("snakePixel");
-    }
-
-    var i = 0;
-    function blinkLoop() {
-        blink();
-        setTimeout(function() {
-            i++;
-            if (i < 10) { blinkLoop();}
-        }, 400);
-    }
-    blinkLoop();
-}
-
-function startGame() {
-    // reset game settings
-    Snake.size = 3;
-    Snake.position = [[20,20],[20,19],[20,18]];
-    Snake.direction = "r";
-    Snake.alive = true;
-    gameSpeed = 100;
-    gamePoints = 0;
-    Food.present = false;
-
-    // start game
-    createBoard();
-    $(".overlay").hide();
-    gameLoop();
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
 }
