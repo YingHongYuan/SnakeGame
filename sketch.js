41a25e9f202f02d68cbc8c867ba012e6fa6a0073
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-05-04 03:21:07 -0700

    `

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var s;
+var scl = 20;
 
 function setup(){
   createCanvas(600,600);
@@ -11,18 +12,14 @@ function draw(){
   s.show();
 }
 
-function Snake(){
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-
-  this.update = function(){
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
-  }
-  this.show = function(){
-    fill(255);
-    rect(this.x,this.y,10,10);
-  }
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    s.dir(0,-1);
+}else if(keyCode === DOWN_ARROW){
+    s.dir(0,1);
+}else if(keyCode === RIGHT_ARROW){
+    s.dir(1,0);
+}else if(keyCode === LEFT_ARROW){
+    s.dir(-1,0);
+}
 }
